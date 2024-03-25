9f13f480ff505de151a73fd8c4de5846fd6a6242
Кулаков Иван Александрович <i.kulakov@otpbank.ru>
2024-03-25 18:28:29 +0300

    playwright test

---
+++ e2e/courses-list.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from '@playwright/test'
+
+test('create delete course list', async ({ page }) => {
+  await page.locator('body').click()
+  await page.goto('http://localhost:3000/')
+  await page.getByPlaceholder('название').click()
+  await page.getByPlaceholder('название').fill('Test course')
+  await page.getByPlaceholder('описание').click()
+  await page.getByPlaceholder('описание').fill('Test description')
+  await page.getByRole('button', { name: 'Добавить' }).click()
+  await expect(
+    page.getByText('Test courseTest descriptionУдалить'),
+  ).toBeVisible()
+
+  await page.getByRole('button', { name: 'Удалить' }).click()
+
+  await expect(
+    page.getByText('Test courseTest descriptionУдалить'),
+  ).not.toBeVisible()
+})
